c178f87430c166b1a494f0a61d1ed23264fff407
Sinemapaydn <sinem.apaydiinn@gmail.com>
2023-10-26 12:29:18 +0300

    Initial Commit

---
+++ WeatherApp-Swift/WeatherApp_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_SwiftApp.swift
+//  WeatherApp-Swift
+//
+//  Created by SinemApaydın on 26.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
